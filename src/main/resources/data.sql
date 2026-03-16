5d5facac86606164eb7f4e63b382602bca66c0c8
chanbinchoi <classy9611@gmail.com>
2026-03-17 00:00:37 +0900

    end

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into student (rollno, name, marks)
+values (101, 'Kiran', 79);
+
+insert into student (rollno, name, marks)
+values (102, 'Harsh', 68);
+
+insert into student (rollno, name, marks)
+values (103, 'Sushil', 82);
